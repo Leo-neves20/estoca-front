1da4c9a5a3f18fd258c3e708b1f7fa6e9ce35e24
Leonardo Neves <leonardo.neves@dbit.srv.br>
2024-12-21 01:22:12 -0300

    chore: adiciona libs e retira arquivos base da criação do next

---
+++ @types/tailwind.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace CSS {
+    interface AtRules {
+      "@tailwind base": string;
+      "@tailwind components": string;
+      "@tailwind utilities": string;
+      "@tailwind base": string;
+      "@tailwind components": string;
+      "@tailwind utilities": string;
+    }
+  }
+}
